003400f8792fb345faacbae7c3c9bb10cd587a8d
Preetham Lawrence Veigas <preetham.lawrence@gmail.com>
2022-05-05 09:17:09 +0530

    inital commit updates

--- client/components/header.js
+++ client/components/header.js
@@ -22,7 +22,7 @@ const Header = ({ currentUser }) => {
     return(
         <nav className="navbar navbar-light bg-light">
             <Link href="/">
-                <a className="navbar-brand">TicketsHub</a>
+                <a className="navbar-brand">TicketingHub</a>
             </Link>
             <div className="d-flex justify-content-end">
                 <ul className="nav d-flex align-items-center">
